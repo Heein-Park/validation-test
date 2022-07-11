e959ac7e1343e9faefcb3ff87271e3b398eba51b
taptap-kor <tyy8282@gmail.com>
2022-07-11 14:24:17 +0900

    const done

---
+++ validation.js
@@ -0,0 +1,7 @@
+const id_space = document.body.querySelector('#ID')
+const pw_space = document.body.querySelector('#PW')
+const pwConf_space = document.body.querySelector('#PW_Con')
+const button_space = document.body.querySelector('#Confirm')
+
+
+console.log(id_space, pw_space, pwConf_space)
